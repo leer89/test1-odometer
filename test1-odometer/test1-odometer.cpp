fb92cc775bf053f6cf7c5d25091c68ad897a46d0
Randy Lee <leer89@gmail.com>
2017-02-08 18:55:23 -0500

    Update test1-odometer.cpp

--- test1-odometer/test1-odometer.cpp
+++ test1-odometer/test1-odometer.cpp
@@ -10,24 +10,27 @@ using namespace std;
 
 int main() {
 
-	// constructor that sets distance and fuel efficiency to 0
-	Odometer test1; 
+	Odometer test1;
 
-	cout << "Initial Odometer: " << endl;
+	cout << "Initial Odometer: 0\nInitial Fuel Efficiency: 27 mpg" << endl;
 	cout << "Miles Driven: " << test1.getMiles() << endl;
 	cout << "Fuel Efficency: " << test1.getFuelEfficiency() << endl << endl;
 
-	cout << "Adding 54.5 miles to odometer" << endl;
-	test1.addMiles(54.5); //add 54.5 miles
+	cout << "How many miles driven trip 2?: 120" << endl;
+	cout << "Adding 120 miles to odometer" << endl;
+	test1.addMiles(120); //120
 	cout << "Miles Driven: " << test1.getMiles() << endl << endl;
 
-	cout << "The consumption rate of 54.5 is: " << endl;
+	cout << "How many miles driven trip 2?: 350" << endl;
+	cout << "Adding 350 miles to odometer" << endl;
+	test1.addMiles(350); //350
+	cout << "Miles Driven: " << test1.getMiles() << endl << endl;
+
+	cout << "The consumption rate of " << test1.getMiles() << " is: " << endl;
 	//find out the consumption rate
 	cout << "Fuel Efficency: " << test1.getConsumption() << " miles per gallon(MPG)" << endl << endl;
 
 	cout << "Reseting the odometer" << endl;
 	test1.resetMiles(); //reset miles
 	cout << "Miles Driven: " << test1.getMiles() << endl;
-
-	return 0;
 }
